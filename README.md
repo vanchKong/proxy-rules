75ebcc289fb8848abee4ba16b71adc94b8729e14
vanch <vanchkong@qq.com>
2024-09-19 11:09:29 +0800

    init

---
+++ README.md
@@ -0,0 +1,75 @@
+<p align="center">
+  <img width="100px" src="https://user-images.githubusercontent.com/35565811/214613019-6fd702b7-445e-4663-8471-f47005241724.png" align="center" alt="GitHub Readme Stats" />
+  <h2 align="center">Clash Rules Lite</h2>
+ 
+  <p align="center">🍒 自定义代理规则，精简匹配规则。</p>
+ 
+  <p align="center">
+    <a href="https://github.com/vanchkong/proxy-rules/blob/master/.github/workflows/release.yml">
+    <img src="https://github.com/vanchkong/proxy-rules/actions/workflows/release.yml/badge.svg" />
+    </a>
+  </p>
+ 
+  <p align="center">
+  </p>
+
+  <p align="center">
+    <a href="https://cdn.jsdelivr.net/gh/vanchkong/proxy-rules@release/applications-rules.txt">Clash 应用规则列表</a> |
+    <a href="https://cdn.jsdelivr.net/gh/vanchkong/proxy-rules@release/custom-direct-rules.txt">Clash 自定义直连列表</a> |
+    <a href="https://cdn.jsdelivr.net/gh/vanchkong/proxy-rules@release/custom-proxy-rules.txt">Clash 自定义代理列表</a> 
+    <br>
+    <a href="https://cdn.jsdelivr.net/gh/vanchkong/proxy-rules@release/applications-rules.list">Loon 应用规则列表</a> |
+    <a href="https://cdn.jsdelivr.net/gh/vanchkong/proxy-rules@release/custom-direct-rules.list">Loon 自定义直连列表</a> |
+    <a href="https://cdn.jsdelivr.net/gh/vanchkong/proxy-rules@release/custom-proxy-rules.list">Loon 自定义代理列表</a>
+  </p>
+
+</p>
+
+### 工具介绍
+
+- 当用户更新规则后，使用 Github Actions 自动将规则缓存到免费 CDN 上
+- 用户在 github 上更新规则后，在 clash 或 Loon 上点击刷新即可拉取更新
+
+### 如何自定义
+
+1. fork 本仓库：[复刻 vanchkong/proxy-rules](https://github.com/vanchkong/proxy-rules/fork)
+2. 触发 GitHub Action 中的 `Generate Rules for Clash` 工作流
+3. 编辑 `*-rules.*` 以自定义规则
+4. 在对应的 Clash 或 Loon 上刷新配置文件
+
+<div align="center">
+  <center>
+    <img style="border-radius: 0.3125em;
+    box-shadow: 0 2px 4px 0 rgba(34,36,38,.12),0 2px 10px 0 rgba(34,36,38,.08);" 
+    src="https://user-images.githubusercontent.com/35565811/184524456-e956ef59-4577-44e9-9b99-4a8684b77e40.png">
+    <br>
+    <div style="color:orange; border-bottom: 1px solid #d9d9d9;
+    display: inline-block;
+    color: #999;
+    padding: 2px;">启动流水线示意图</div>
+  </center>
+</div>
+
+Tips:
+
+> a. 可通过访问进行验证 `https://cdn.jsdelivr.net/gh/{你的GITHUB用户名}/proxy-rules@release/`  
+> c. **该仓中以 rules.\* 结尾的文件，都会缓存到 jsdelivr CDN 中，可以自定义！**
+
+### CDN 缓存没有更新怎么办？
+
+> 这是因为 jsdelivr CDN 缓存的原因，一般来说是 24 小时刷新缓存，但是这样太慢了！  
+> 不过 jsdelivr CDN 也提供手动刷新缓存的方法：
+
+```
+# 假设你的文件 URL 是这样：
+https://cdn.jsdelivr.net/xxx/xxx...
+
+# 那么把域名中的 cdn 改为 purge 即可：
+https://purge.jsdelivr.net/xxx/xxx...
+```
+
+然后访问这个文件新 URL 就会提示你刷新成功！
+
+### 覆写
+
+我准备了 [clash 的 扩展脚本](./clash.js)、[loon 的 覆写配置](./loon.sh)，
---
+++ clash.js
@@ -0,0 +1,597 @@
+// 'filter':
+// 	'AD|🇦🇩|AE|🇦🇪|阿拉伯|AF|🇦🇫|AG|🇦🇬|AL|🇦🇱|AM|🇦🇲|AO|🇦🇴|AR|🇦🇷|阿根廷|AT|🇦🇹|AU|🇦🇺|澳大利亚|AZ|🇦🇿|BA|🇧🇦|BB|🇧🇧|BD|🇧🇩|孟加拉|BE|🇧🇪|BF|🇧🇫|BG|🇧🇬|BH|🇧🇭|BI|🇧🇮|BJ|🇧🇯|BN|🇧🇳|BO|🇧🇴|BR|🇧🇷|巴西
+// |BS|🇧🇸|BT|🇧🇹|BW|🇧🇼|BZ|🇧🇿|CA|🇨🇦|加拿大|CD|🇨🇩|CF|🇨🇫|CG|🇨🇬|CH|🇨🇭|瑞士|CI|🇨🇮|CL|🇨🇱|CM|🇨🇲|CO|🇨🇴|CR|🇨🇷|CV|🇨🇻|CY|🇨🇾|CZ|🇨🇿|DE|🇩🇪|德国|DJ|🇩🇯|DK|🇩🇰|DM|🇩🇲|DO|🇩🇴|DZ|🇩🇿|EC|🇪🇨|EE|🇪🇪|爱沙尼亚
+// |EG|🇪🇬|埃及|ER|🇪🇷|ES|🇪🇸|ET|🇪🇹|FI|🇫🇮|芬兰|FJ|🇫🇯|FM|🇫🇲|FR|🇫🇷|法国|GA|🇬🇦|加蓬|GB|🇬🇧|英国|GD|🇬🇩|GE|🇬🇪|GH|🇬🇭|GM|🇬🇲|GN|🇬🇳|GQ|🇬🇶|GR|🇬🇷|GT|🇬🇹|GW|🇬🇼|GY|港|HK|Hong Kong|HongKong|🇭🇰|🇬🇾|HN|🇭🇳|HR|🇭🇷|HT|🇭🇹|HU|🇭🇺|ID|🇮🇩|印尼
+// |IE|🇮🇪|IL|🇮🇱|IN|🇮🇳|印度|IQ|🇮🇶|伊拉克|IS|🇮🇸|IT|🇮🇹|意大利|JM|🇯🇲|JO|🇯🇴|JP|🇯🇵|日本|Japan|KE|🇰🇪|KG|🇰🇬|KH|🇰🇭|柬埔寨|KI|🇰🇮|KM|🇰🇲|KN|🇰🇳|韩国|KR|Korea|KOR|🇰🇷|KW|🇰🇼|KZ|🇰🇿|哈萨克|LA|🇱🇦|老挝
+// |LB|🇱🇧|LC|🇱🇨|LI|🇱🇮|LK|🇱🇰|LR|🇱🇷|LS|🇱🇸|LT|🇱🇹|LU|🇱🇺|LV|🇱🇻|LY|🇱🇾|MA|🇲🇦|MC|🇲🇨|MD|🇲🇩|ME|🇲🇪|MG|🇲🇬|MH|🇲🇭|MK|🇲🇰|ML|🇲🇱|MM|🇲🇲|缅甸|MN|🇲🇳|MO|🇲🇴|澳门|MR|🇲🇷|MT|🇲🇹|MU|🇲🇺|MV|🇲🇻|MW|🇲🇼|MX|🇲🇽
+// |MY|🇲🇾|马来西亚|MZ|🇲🇿|NA|🇳🇦|NE|🇳🇪|NG|🇳🇬|尼日利亚|NI|🇳🇮|NL|🇳🇱|荷兰|NO|🇳🇴|NP|🇳🇵|NR|🇳🇷|NZ|🇳🇿|OM|🇴🇲|PA|🇵🇦|PE|🇵🇪|PG|🇵🇬|PH|🇵🇭|PK|🇵🇰|巴基斯坦|PL|🇵🇱|PS|🇵🇸|PT|🇵🇹|PW|🇵🇼|PY|🇵🇾|QA|🇶🇦|RO|🇷🇴|RS|🇷🇸
+// |RUS|🇷🇺|俄罗斯|RW|🇷🇼|SA|🇸🇦|SB|🇸🇧|SC|🇸🇨|SD|🇸🇩|SE|🇸🇪|SG|🇸🇬|新加坡|Singapore|SI|🇸🇮|SK|🇸🇰|SL|🇸🇱|SM|🇸🇲|SN|🇸🇳|SO|🇸🇴|SR|🇸🇷|SS|🇸🇸|ST|🇸🇹|SV|🇸🇻|SZ|🇸🇿|TD|🇹🇩|TG|🇹🇬|TH|🇹🇭|泰国|TJ|🇹🇯|TL|🇹🇱|TM|🇹🇲|TN|🇹🇳|TO|🇹🇴
+// |TR|🇹🇷|土耳其|TT|🇹🇹|TV|🇹🇻|台|台湾|TAIWAN|TW|TAI WAN|🇹🇼|TZ|🇹🇿|UA|🇺🇦|乌克兰|UG|🇺🇬|United States|America|USA|US|🇺🇸|美国|UY|🇺🇾|UZ|🇺🇿|VA|🇻🇦|VC|🇻🇨|VN|🇻🇳|越南|VU|🇻🇺|WS|🇼🇸|YE|🇾🇪|ZA|🇿🇦|ZM|🇿🇲|ZW|🇿🇼',
+
+// https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Telegram/Telegram.yaml
+// https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/Telegram/Telegram.yaml
+// https://ghproxy.com/https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Telegram/Telegram.yaml
+
+// 国内DNS服务器
+const domesticNameservers = [
+	'https://dns.alidns.com/dns-query', // 阿里云公共DNS
+	'https://doh.pub/dns-query', // 腾讯DNSPod
+	'https://doh.360.cn/dns-query', // 360安全DNS
+]
+// 国外DNS服务器
+const foreignNameservers = [
+	'https://1.1.1.1/dns-query', // Cloudflare(主)
+	'https://1.0.0.1/dns-query', // Cloudflare(备)
+	'https://208.67.222.222/dns-query', // OpenDNS(主)
+	'https://208.67.220.220/dns-query', // OpenDNS(备)
+	'https://194.242.2.2/dns-query', // Mullvad(主)
+	'https://194.242.2.3/dns-query', // Mullvad(备)
+]
+// DNS配置
+const dnsConfig = {
+	'enable': true,
+	'listen': '0.0.0.0:1053',
+	'ipv6': true,
+	'use-system-hosts': false,
+	'cache-algorithm': 'arc',
+	'enhanced-mode': 'fake-ip',
+	'fake-ip-range': '198.18.0.1/16',
+	'fake-ip-filter': [
+		// 本地主机/设备
+		'+.lan',
+		'+.local',
+		// Windows网络出现小地球图标
+		'+.msftconnecttest.com',
+		'+.msftncsi.com',
+		// QQ快速登录检测失败
+		'localhost.ptlogin2.qq.com',
+		'localhost.sec.qq.com',
+		// 微信快速登录检测失败
+		'localhost.work.weixin.qq.com',
+	],
+	'default-nameserver': ['223.5.5.5', '119.29.29.29', '1.1.1.1', '8.8.8.8'],
+	'nameserver': [...domesticNameservers, ...foreignNameservers],
+	'proxy-server-nameserver': [...domesticNameservers, ...foreignNameservers],
+	'nameserver-policy': {
+		'geosite:private,cn,geolocation-cn': domesticNameservers,
+		'geosite:google,youtube,telegram,gfw,geolocation-!cn': foreignNameservers,
+	},
+}
+// 规则集通用配置
+const ruleProviderCommon = {
+	type: 'http',
+	format: 'yaml',
+	interval: 86400,
+}
+// 规则集配置
+const ruleProviders = {
+	// 去广告 ---------------------------------------------------------
+	'advertisingLite': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/AdvertisingLite/AdvertisingLite.yaml',
+		path: './ruleset/advertisingLite.yaml',
+	},
+	// 巴哈姆特 ------------------------------------------------------------
+	'Bahamut': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/Bahamut/Bahamut.yaml',
+		path: './ruleset/Bahamut.yaml',
+	},
+	// Netflix ------------------------------------------------------------
+	'Netflix': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/Netflix/Netflix_Classical.yaml',
+		path: './ruleset/Netflix_Classical.yaml',
+	},
+	// AI ------------------------------------------------------------
+	'Gemini': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/Gemini/Gemini.yaml',
+		path: './ruleset/Gemini.yaml',
+	},
+	'Copilot': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/Copilot/Copilot.yaml',
+		path: './ruleset/Copilot.yaml',
+	},
+	'Claude': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/Claude/Claude.yaml',
+		path: './ruleset/Claude.yaml',
+	},
+	'BardAI': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/BardAI/BardAI.yaml',
+		path: './ruleset/BardAI.yaml',
+	},
+	'OpenAI': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/OpenAI/OpenAI.yaml',
+		path: './ruleset/OpenAI.yaml',
+	},
+	// 大陆域名、IP --------------------------------------------------------
+	'ChinaMax_Classical_No_Resolve': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/ChinaMax/ChinaMax_Classical_No_Resolve.yaml',
+		path: './ruleset/ChinaMax_Classical_No_Resolve.yaml',
+	},
+	// 谷歌ip ******************************************************
+	'GoogleIP': {
+		...ruleProviderCommon,
+		behavior: 'ipcidr',
+		url: 'https://mirror.ghproxy.com/https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/google.yaml',
+		path: './ruleset/GoogleIP.yaml',
+	},
+	// 谷歌域名 --------------------------------------------------------
+	'Google': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/Google/Google.yaml',
+		path: './ruleset/Google.yaml',
+	},
+	// YouTube域名 --------------------------------------------------------
+	'YouTube': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/YouTube/YouTube.yaml',
+		path: './ruleset/YouTube.yaml',
+	},
+	// 非大陆域名 ******************************************************
+	'geolocation-!cn': {
+		...ruleProviderCommon,
+		behavior: 'domain',
+		url: 'https://mirror.ghproxy.com/https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/geolocation-!cn.yaml',
+		path: './ruleset/geolocation-!cn.yaml',
+	},
+	// 电报域名 -------------------------------------------------------
+	'Telegram': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/Telegram/Telegram.yaml',
+		path: './ruleset/Telegram.yaml',
+	},
+	// 电报IP ******************************************************
+	// 'telegram_ip': {
+	// 	...ruleProviderCommon,
+	// 	behavior: 'ipcidr',
+	// 	url: 'https://mirror.ghproxy.com/https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geoip/telegram.yaml',
+	// 	path: './ruleset/telegram_ip.yaml',
+	// },
+	// steam ---------------------------------------------------------
+	'Steam': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/Steam/Steam.yaml',
+		path: './ruleset/Steam.yaml',
+	},
+	// steamCN ---------------------------------------------------------
+	'SteamCN_No_Resolve': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/SteamCN/SteamCN_No_Resolve.yaml',
+		path: './ruleset/SteamCN_No_Resolve.yaml',
+	},
+	// Apple ---------------------------------------------------------
+	'Apple_Classical_No_Resolve': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/Apple/Apple_Classical_No_Resolve.yaml',
+		path: './ruleset/Apple_Classical_No_Resolve.yaml',
+	},
+	// Microsoft ---------------------------------------------------------
+	'Microsoft_No_Resolve': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/Microsoft/Microsoft_No_Resolve.yaml',
+		path: './ruleset/Microsoft_No_Resolve.yaml',
+	},
+	// 局域网 ---------------------------------------------------------
+	'Lan_No_Resolve': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/Lan/Lan_No_Resolve.yaml',
+		path: './ruleset/Lan_No_Resolve.yaml',
+	},
+	//  applications ******************************************************
+	'applications': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/vanchkong/proxy-rules@release/applications-rules.txt',
+		path: './ruleset/applications.yaml',
+	},
+	'Github': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/blackmatrix7/ios_rule_script@master/rule/Clash/GitHub/GitHub.yaml',
+		path: './ruleset/GitHub.yaml',
+	},
+	// 自定义 ******************************************************
+	'custom-proxy': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/vanchkong/proxy-rules@release/custom-proxy-rules.txt',
+		path: './ruleset/custom-proxy.yaml',
+	},
+	'custom-direct': {
+		...ruleProviderCommon,
+		behavior: 'classical',
+		url: 'https://cdn.jsdelivr.net/gh/vanchkong/proxy-rules@release/custom-direct-rules.txt',
+		path: './ruleset/custom-direct.yaml',
+	},
+}
+
+// 规则
+const rules = [
+	'RULE-SET,applications,全局直连 🏠',
+	'RULE-SET,Lan_No_Resolve,全局直连 🏠',
+	'RULE-SET,custom-direct,全局直连 🏠',
+	'RULE-SET,advertisingLite,全局拦截 🚧',
+	'RULE-SET,Microsoft_No_Resolve,微软服务 Ⓜ️',
+	'RULE-SET,Apple_Classical_No_Resolve,苹果服务 🍎',
+	'RULE-SET,SteamCN_No_Resolve,全局直连 🏠',
+	'RULE-SET,Steam,蒸汽平台 🎮',
+	'RULE-SET,Bahamut,巴哈姆特 📺',
+	'RULE-SET,Netflix,奈飞服务 📺',
+	'RULE-SET,Gemini,人工智能 🤖',
+	'RULE-SET,Copilot,人工智能 🤖',
+	'RULE-SET,Claude,人工智能 🤖',
+	'RULE-SET,BardAI,人工智能 🤖',
+	'RULE-SET,OpenAI,人工智能 🤖',
+	'RULE-SET,Telegram,电报服务 💬',
+	// 'RULE-SET,telegram_ip,电报服务 💬',
+	'RULE-SET,Google,谷歌服务 🇺🇸',
+	'RULE-SET,GoogleIP,谷歌服务 🇺🇸',
+	'RULE-SET,YouTube,谷歌服务 🇺🇸',
+	'RULE-SET,Github,地区选择 🌍',
+	'RULE-SET,geolocation-!cn,地区选择 🌍',
+	'RULE-SET,custom-proxy,地区选择 🌍',
+	'RULE-SET,ChinaMax_Classical_No_Resolve,全局直连 🏠',
+	'MATCH,漏网之鱼 🐟',
+]
+// 代理组通用配置
+const groupBaseOption = {
+	'interval': 300,
+	'timeout': 3000,
+	'url': 'https://www.google.com/generate_204',
+	'lazy': true,
+	'max-failed-times': 3,
+	'hidden': false,
+}
+
+// 程序入口
+function main(config) {
+	const proxyCount = config?.proxies?.length ?? 0
+	const proxyProviderCount = typeof config?.['proxy-providers'] === 'object' ? Object.keys(config['proxy-providers']).length : 0
+	if (proxyCount === 0 && proxyProviderCount === 0) {
+		throw new Error('配置文件中未找到任何代理')
+	}
+
+	if (config?.['proxy-providers'] === 'object') {
+		console.log(config?.['proxy-providers'])
+	}
+
+	const sortArrByOrder = (arr, order, key) => {
+		let filterArr = []
+		let leftArr = []
+		// leftArr 作用是当order中不存在相应value时，最后返回时排序在最后 如果需要排序在最前面，则取消leftArr这一步，只调用sort方法即可
+		if (key) {
+			filterArr = arr.filter((item) => {
+				return order.some((item2) => {
+					return item[key].toUpperCase().indexOf(item2) !== -1
+				})
+			})
+			leftArr = arr.filter((item) => {
+				return order.every((item2) => {
+					return item[key].toUpperCase().indexOf(item2) === -1
+				})
+			})
+		} else {
+			filterArr = arr.filter((item) => order.indexOf(item) !== -1)
+			leftArr = arr.filter((item) => order.indexOf(item) === -1)
+		}
+		filterArr.sort((a, b) => {
+			if (key) {
+				// return order.indexOf(a[key].toUpperCase()) - order.indexOf(b[key].toUpperCase())
+				let index1 = 0
+				let index2 = 0
+				order.some((item2, index) => {
+					index1 = index
+					return a[key].toUpperCase().indexOf(item2) !== -1
+				})
+				order.some((item2, index) => {
+					index2 = index
+					return b[key].toUpperCase().indexOf(item2) !== -1
+				})
+				return index1 - index2
+			} else {
+				return order.indexOf(a) - order.indexOf(b)
+			}
+		})
+		return filterArr.concat(leftArr)
+	}
+
+	const sortedAllProxy = sortArrByOrder(
+		config?.proxies,
+		[
+			'剩',
+			'到',
+			'重',
+			'🇭🇰',
+			'HK',
+			'HONG KONG',
+			'HongKong',
+			'港',
+			'United States',
+			'America',
+			'USA',
+			'US',
+			'🇺🇸',
+			'美国',
+			'TW',
+			'TAIWAN',
+			'TAI WAN',
+			'台',
+			'台湾',
+			'🇹🇼',
+			'JP',
+			'JAPAN',
+			'🇯🇵',
+			'日本',
+			'SG',
+			'Singapore',
+			'🇸🇬',
+			'新加坡',
+			'韩国',
+			'KR',
+			'Korea',
+			'KOR',
+			'🇰🇷',
+		],
+		'name',
+	)
+
+	// console.log(sortedAllProxy)
+
+	const allProxy = []
+	const unpopularProxy = []
+	const rep = /剩|到|重|United States|America|USA|US|🇺🇸|美国|SG|🇸🇬|新加坡|Singapore|JP|🇯🇵|日本|Japan|港|HK|Hong Kong|HongKong|🇭🇰|台|台湾|TAIWAN|TW|TAI WAN|🇹🇼|韩国|KR|Korea|KOR|🇰🇷/i
+	sortedAllProxy.forEach((item) => {
+		allProxy.push(item['name'])
+		if (!rep.test(item['name'])) {
+			unpopularProxy.push(item['name'])
+		}
+	})
+
+	// 覆盖原配置中DNS配置
+	config['dns'] = dnsConfig
+
+	// 覆盖原配置中的代理组
+	config['proxy-groups'] = [
+		// 地区选择
+		{
+			...groupBaseOption,
+			name: '地区选择 🌍',
+			type: 'select',
+			proxies: ['自动选择 📡', '手动选择 👌', '香港节点 🇭🇰', '日本节点 🇯🇵', '美国节点 🇺🇸', '台湾节点 🇨🇳', '新加坡节点 🇸🇬', '韩国节点 🇰🇷', '冷门地区 ❄️', '全局直连 🏠'],
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Area.png',
+		},
+		// 冷门地区
+		{
+			...groupBaseOption,
+			name: '冷门地区 ❄️',
+			type: 'select',
+			proxies: [...unpopularProxy, '全局直连 🏠'],
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/World_Map.png',
+		},
+		// 所有节点
+		{
+			...groupBaseOption,
+			name: '手动选择 👌',
+			type: 'select',
+			proxies: [...allProxy],
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Available.png',
+		},
+		// AIGC
+		{
+			...groupBaseOption,
+			name: '人工智能 🤖',
+			type: 'select',
+			proxies: ['日本节点 🇯🇵', '美国节点 🇺🇸', '地区选择 🌍', '手动选择 👌'],
+			filter: 'US|🇺🇸|美国|United States|TW|🇹🇼|台湾',
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Bot.png',
+		},
+		// 巴哈姆特
+		{
+			...groupBaseOption,
+			name: '巴哈姆特 📺',
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Bahamut.png',
+			type: 'select',
+			proxies: ['台湾节点 🇨🇳', '地区选择 🌍', '手动选择 👌', '全局直连 🏠'],
+		},
+		// 谷歌服务
+		{
+			...groupBaseOption,
+			name: '谷歌服务 🇺🇸',
+			type: 'select',
+			proxies: ['地区选择 🌍', '手动选择 👌', '香港节点 🇭🇰', '日本节点 🇯🇵', '美国节点 🇺🇸', '台湾节点 🇨🇳', '新加坡节点 🇸🇬', '韩国节点 🇰🇷', '冷门地区 ❄️'],
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Google_Search.png',
+		},
+		// 蒸汽平台 🎮
+		{
+			...groupBaseOption,
+			name: '蒸汽平台 🎮',
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Steam.png',
+			type: 'select',
+			proxies: ['地区选择 🌍', '手动选择 👌', '香港节点 🇭🇰', '日本节点 🇯🇵', '美国节点 🇺🇸', '台湾节点 🇨🇳', '新加坡节点 🇸🇬', '韩国节点 🇰🇷', '冷门地区 ❄️'],
+		},
+		// 蒸汽平台CN 🎮
+		// {
+		// 	...groupBaseOption,
+		// 	name: '蒸汽平台CN 🎮',
+		// 	icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Steam.png',
+		// 	type: 'select',
+		// 	proxies: ['全局直连 🏠'],
+		// 	hidden: true,
+		// },
+		// 奈飞服务 📺
+		{
+			...groupBaseOption,
+			name: '奈飞服务 📺',
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Netflix.png',
+			type: 'select',
+			proxies: ['地区选择 🌍', '手动选择 👌', '香港节点 🇭🇰', '日本节点 🇯🇵', '美国节点 🇺🇸', '台湾节点 🇨🇳', '新加坡节点 🇸🇬', '韩国节点 🇰🇷', '冷门地区 ❄️'],
+		},
+		// 电报服务 💬
+		{
+			...groupBaseOption,
+			name: '电报服务 💬',
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Telegram.png',
+			type: 'select',
+			proxies: ['地区选择 🌍', '手动选择 👌', '香港节点 🇭🇰', '日本节点 🇯🇵', '美国节点 🇺🇸', '台湾节点 🇨🇳', '新加坡节点 🇸🇬', '韩国节点 🇰🇷', '冷门地区 ❄️'],
+		},
+		// 微软服务 Ⓜ️
+		{
+			...groupBaseOption,
+			name: '微软服务 Ⓜ️',
+			type: 'select',
+			proxies: ['全局直连 🏠', '地区选择 🌍', '手动选择 👌'],
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Microsoft.png',
+		},
+		// 苹果服务 🍎
+		{
+			...groupBaseOption,
+			name: '苹果服务 🍎',
+			type: 'select',
+			proxies: ['全局直连 🏠', '地区选择 🌍', '手动选择 👌'],
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Apple.png',
+		},
+		// 全局直连 🏠
+		{
+			...groupBaseOption,
+			name: '全局直连 🏠',
+			type: 'select',
+			proxies: ['DIRECT', 'REJECT'],
+			url: 'http://wifi.vivo.com.cn/generate_204',
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Direct.png',
+			hidden: true,
+		},
+		// 全局拦截 🚧
+		{
+			...groupBaseOption,
+			name: '全局拦截 🚧',
+			type: 'select',
+			proxies: ['REJECT', 'DIRECT'],
+			url: 'http://wifi.vivo.com.cn/generate_204',
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/AdBlack.png',
+			hidden: true,
+		},
+		// 漏网之鱼 🐟
+		{
+			...groupBaseOption,
+			name: '漏网之鱼 🐟',
+			type: 'select',
+			proxies: ['地区选择 🌍', '手动选择 👌', '全局直连 🏠'],
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Final.png',
+		},
+		// GLOBAL
+		{
+			name: 'GLOBAL',
+			type: 'select',
+			proxies: ['地区选择 🌍', '手动选择 👌', '全局直连 🏠', '自动选择 📡'],
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Global.png',
+			hidden: true,
+		},
+		// 自动选择 📡
+		{
+			...groupBaseOption,
+			name: '自动选择 📡',
+			type: 'url-test',
+			tolerance: 50,
+			proxies: [...allProxy],
+			icon: 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Auto.png',
+			hidden: true,
+		},
+		// 香港节点 🇭🇰
+		{
+			...groupBaseOption,
+			'tolerance': 50,
+			'name': '香港节点 🇭🇰',
+			'type': 'url-test',
+			'filter': '(?i)港|HK|Hong Kong|HongKong|🇭🇰',
+			'include-all': true,
+			'icon': 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Hong_Kong.png',
+			'hidden': true,
+		},
+		// 台湾节点 🇨🇳
+		{
+			...groupBaseOption,
+			'tolerance': 50,
+			'name': '台湾节点 🇨🇳',
+			'type': 'url-test',
+			'filter': '(?i)台|台湾|TAIWAN|TW|TAI WAN|🇹🇼',
+			'include-all': true,
+			'icon': 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Taiwan.png',
+			'hidden': true,
+		},
+		// 美国节点 🇺🇸
+		{
+			...groupBaseOption,
+			'tolerance': 50,
+			'name': '美国节点 🇺🇸',
+			'type': 'url-test',
+			'filter': '(?i)United States|America|USA|US|🇺🇸|美国',
+			'include-all': true,
+			'icon': 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/United_States.png',
+			'hidden': true,
+		},
+		// 日本节点 🇯🇵
+		{
+			...groupBaseOption,
+			'tolerance': 50,
+			'name': '日本节点 🇯🇵',
+			'type': 'url-test',
+			'filter': '(?i)JP|🇯🇵|日本|Japan',
+			'include-all': true,
+			'icon': 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Japan.png',
+			'hidden': true,
+		},
+		// 新加坡节点 🇸🇬
+		{
+			...groupBaseOption,
+			'tolerance': 50,
+			'name': '新加坡节点 🇸🇬',
+			'type': 'url-test',
+			'filter': '(?i)SG|🇸🇬|新加坡|Singapore',
+			'include-all': true,
+			'icon': 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Singapore.png',
+			'hidden': true,
+		},
+		// 韩国节点 🇰🇷
+		{
+			...groupBaseOption,
+			'tolerance': 50,
+			'name': '韩国节点 🇰🇷',
+			'type': 'url-test',
+			'filter': '(?i)韩国|KR|Korea|KOR|🇰🇷',
+			'include-all': true,
+			'icon': 'https://fastly.jsdelivr.net/gh/Koolson/Qure@master/IconSet/Color/Korea.png',
+			'hidden': true,
+		},
+	]
+
+	// 覆盖原配置中的规则
+	config['rule-providers'] = ruleProviders
+	config['rules'] = rules
+
+	// 返回修改后的配置
+	return config
+}
